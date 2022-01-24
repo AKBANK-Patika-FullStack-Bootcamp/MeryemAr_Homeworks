7949df1f52a0171a69b2d144e2a62c77967dedb7
Meryem Sevval Ar <msevvalar@gmail.com>
2022-01-24 22:06:00 +0300

    token logger

---
+++ Week 4/DAL/HeroNames.cs
@@ -0,0 +1,9 @@
+﻿namespace DAL
+{
+    public class HeroNames
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
